3d7779b17a7005635dd8e71b7c1df62b3abe44e0
vidya.priyadarshini <vidya.priyadarshini@flipkart.com>
2016-07-23 12:04:58 +0530

    linkedlist

---
+++ hackerrank/src/main/java/linkedlist/Node.java
@@ -0,0 +1,13 @@
+package linkedlist;
+
+/**
+ * Created by vidya.priyadarshini on 23/07/16.
+ */
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
