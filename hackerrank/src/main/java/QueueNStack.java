b208c582f97fe0cc451c6e20d38a688ce27b9495
vidya.priyadarshini <vidya.priyadarshini@flipkart.com>
2016-07-23 12:53:46 +0530

    queuenstack

---
+++ hackerrank/src/main/java/QueueNStack.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+/**
+ * Created by vidya.priyadarshini on 23/07/16.
+ */
+class QueueNStack {
+
+    Stack<Character> stack = new Stack<Character>();
+    Queue<Character> queue = new LinkedList<Character>();
+
+    void pushCharacter(Character c) {
+
+        if(Character.isUpperCase(c)) System.exit(0);
+        stack.push(c);
+
+    }
+
+    void enqueueCharacter(Character c) {
+
+        if(Character.isUpperCase(c)) System.exit(0);
+        queue.add(c);
+    }
+
+    Character popCharacter() {
+
+        return stack.pop();
+    }
+
+    Character dequeueCharacter() {
+        return queue.remove();
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        scan.close();
+
+        // Convert input String to an array of characters:
+        char[] s = input.toCharArray();
+
+        // Create a Solution object:
+        QueueNStack p = new QueueNStack();
+
+        // Enqueue/Push all chars to their respective data structures:
+        for (char c : s) {
+            p.pushCharacter(c);
+            p.enqueueCharacter(c);
+        }
+
+        // Pop/Dequeue the chars at the head of both data structures and compare them:
+        boolean isPalindrome = true;
+        for (int i = 0; i < s.length/2; i++) {
+            if (p.popCharacter() != p.dequeueCharacter()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        //Finally, print whether string s is palindrome or not.
+        System.out.println( "The word, " + input + ", is "
+                + ( (!isPalindrome) ? "not a palindrome." : "a palindrome." ) );
+    }
+}
+
